c74de30661235f523b0ffaf28a174a06cf04230c
csmiklos2003 <csmiklos2003@gmail.com>
2025-11-10 18:23:50 +0100

    Reszponzív téma alkalmazása

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <section>
+        <header class="major">
+            <h2>Üdvözöllek az oldalon!</h2>
+        </header>
+        <p>
+            Ez a főoldal tartalma, amit a <strong>Escape Velocity</strong> reszponzív sablon jelenít meg.
+            Itt lesznek majd a dinamikus elemek, adatbázisból származó adatok, stb.
+        </p>
+    </section>
+@endsection
